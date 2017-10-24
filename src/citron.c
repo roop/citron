736ade10c17bf336f36b427846e8174b80435631
Roopesh Chander <roop@roopc.net>
2017-10-24 11:33:21 +0530

    citron.c: Update copyright

--- src/citron.c
+++ src/citron.c
@@ -1,33 +1,35 @@
 /*
- * Original Lemon LALR(1) parser generator:
- *
- *     Author disclaimed copyright
- *
- *     Public domain code.
- *
- * Modifications to generate Swift output:
- *
- *     Copyright (C) 2017 Roopesh Chander <roop@roopc.net>
- *
- *     Permission is hereby granted, free of charge, to any person obtaining a
- *     copy of this software and associated documentation files (the
- *     "Software"), to deal in the Software without restriction, including
- *     without limitation the rights to use, copy, modify, merge, publish,
- *     distribute, sublicense, and/or sell copies of the Software, and to
- *     permit persons to whom the Software is furnished to do so, subject to
- *     the following conditions:
- *
- *     The above copyright notice and this permission notice shall be included
- *     in all copies or substantial portions of the Software.
- *
- *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- *     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- *     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+
+Lemon: LALR(1) parser generator that generates a parser in C
+
+    Author disclaimed copyright
+
+    Public domain code.
+
+Citron: Modifications to Lemon to generate a parser in Swift
+
+    Copyright (C) 2017 Roopesh Chander <roop@roopc.net>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
