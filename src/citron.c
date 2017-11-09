84ac13be7e41beeed74cf7aaefd0ee08e0678dcb
Roopesh Chander <roop@roopc.net>
2017-11-09 13:30:46 +0530

    citron.c: s/.baseOfStack/.yyBaseOfStack/g;

--- src/citron.c
+++ src/citron.c
@@ -3801,7 +3801,7 @@ void print_symbol_enumeration(
 
   /* Print out the definition of Symbol as a "union"-ish enum */
   fprintf(out,"    enum Symbol {\n");
-  fprintf(out,"        case baseOfStack\n");
+  fprintf(out,"        case yyBaseOfStack\n");
   fprintf(out,"        case yy0(value: Token)\n");
   for(i=0; i<arraysize; i++){
     if( types[i]==0 ) continue;
@@ -4270,7 +4270,7 @@ void ReportTable(
   fprintf(out, "    // Stack\n\n");
 
   fprintf(out, "    var yyStack: [(state: Int , symbolCode: SymbolCode, symbol: Symbol)]  = [\n");
-  fprintf(out, "        (state: 0, symbolCode: 0, symbol: .baseOfStack)\n");
+  fprintf(out, "        (state: 0, symbolCode: 0, symbol: .yyBaseOfStack)\n");
   fprintf(out, "    ]\n");
   fprintf(out, "    var maxStackSize: Int? = nil\n");
   fprintf(out, "    var onStackOverflow: (() -> Void)? = nil\n\n");
