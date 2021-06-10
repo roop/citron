3d40591bf57af40c3f4999cbd34c54861e6ff484
Roopesh Chander <roop@roopc.net>
2021-06-10 15:02:14 +0530

    examples/expr_ec_spm: Use GitHub URL in Package.swift

--- examples/expr_ec_spm/Package.swift
+++ examples/expr_ec_spm/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "../../"),
+        .package(url: "https://github.com/roop/citron.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
