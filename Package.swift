c0de4318b3d55bb31a8bbca650bb2a8a425fac87
Dave Abrahams <dabrahams@adobe.com>
2022-05-13 16:11:40 -0700

    Update swift tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["CitronLexerModule"]),
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "citron"),
         .target(
             name: "CitronParserModule",
